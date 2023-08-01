06062a80f3386c12cbc3c664b65553a767fc5f05
barbonara <paul.colognese@gmail.com>
2023-08-01 13:18:00 +0000

    refactoring goal detection

--- procgen-tools-custom-mazes/procgen_tools/models.py
+++ procgen-tools-custom-mazes/procgen_tools/models.py
@@ -231,6 +231,8 @@ def human_readable_actions(probs: np.ndarray) -> dict:
 def load_policy(model_file: str, action_size: int, device = None) -> CategoricalPolicy:
     assert type(action_size) == int
 
+    print(model_file)
+
     checkpoint = torch.load(model_file, map_location=device)
 
     # CURSED. scale varies between models trained on the lauro vs. master branch. 
