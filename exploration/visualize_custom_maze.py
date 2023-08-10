327987b430666fac10471e4fd1a9b449c3eb3e85
barbonara <paul.colognese@gmail.com>
2023-08-10 14:30:51 +0000

    adding model evaluation method and goal extration method in exploration

--- exploration/visualize_custom_maze.py
+++ exploration/visualize_custom_maze.py
@@ -9,17 +9,21 @@ import matplotlib.pyplot as plt
 
 #venv = ProcgenGym3Env(num=1, env_name='maze')
 #env = gym.make('procgen:procgen-maze_yellowstar_redgem-v0')
-env = gym.make('procgen:procgen-maze_redgem_yellowstar-v0')
+#env = gym.make('procgen:procgen-maze_redgem_yellowstar-v0')
 
 
-obs = env.reset()
-plt.imshow(obs)
+# obs = env.reset()
+# plt.imshow(obs)
 
 # img = venv.env.get_info()[0]["rgb"]
 # plt.show(img)
 
-venv = maze.create_venv(1, 0, 0, env_name='maze_redgem_yellowstar')
-visualization.visualize_venv(venv, 0)
+venv = maze.create_venv(1, 0, 0, env_name='maze_yellowstar_redgem')
+visualization.visualize_venv(venv, 0, ax_size=10, render_padding=True)
 
+
+
+
+# %%
 
 # %%
